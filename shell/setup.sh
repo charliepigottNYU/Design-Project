9ff1e7d8839b0538356e8904620ee59ed9e81e87
charliepigottNYU <cdp332@nyu.edu>
2017-10-18 13:19:07 -0400

    initial table setup for database

---
+++ shell/setup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+export PGPASSWORD=CS4523
+psql -U postgres -d postgres -a -f ../sql/setup.sql 
---
+++ sql/setup.sql
@@ -0,0 +1,5 @@
+CREATE TABLE user_info(
+    username        TEXT PRIMARY KEY    NOT NULL,
+    password        TEXT                NOT NULL
+
+);
