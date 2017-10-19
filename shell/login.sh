a4ff774e50b8824d386012b35034e7a0515dae72
Brian Quinn <bjq207@nyu.edu>
2017-10-19 14:03:52 -0400

    shell script for login minor shell change for signup

---
+++ shell/login.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+export PGPASSWORD=CS4523
+usage="Usage: -u <username>"
+
+while getopts ":u:" opt; do
+    case $opt in
+        u) username="$OPTARG"
+        ;;
+        \?) echo $usage; exit 1;
+        ;;
+    esac
+done
+
+if [-z $username ]; 
+then
+    echo $usage;
+    exit 1
+fi
+
+mkdir -p ../tmp
+
+cat ../sql/templates/get_password.template | sed -e "s/<username>/$username/g"  > ../tmp/get_password.sql
+
+psql -U postgres -f ../tmp/get_password.sql -d postgress -t -v "ON_ERROR_STOP=1"
+
+psql_error_code=$?
+
+rm ../tmp/get_password.sql
+
+if [ $psql_error_code -ne 0 ];
+then
+    exit $psql_error_code;
+fi
+
+exit $?
