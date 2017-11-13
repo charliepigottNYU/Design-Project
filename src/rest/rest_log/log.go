cb685aa845a7775c99e0b052a2dd6bc1491d949f
charlie <cdp332@nyu.edu>
2017-11-13 15:04:27 -0500

    go log code

---
+++ src/rest/rest_log/log.go
@@ -0,0 +1,27 @@
+package rest_log
+
+import (
+    "log"
+    "os"
+)
+
+const (
+    INFO = iota
+    WARNING
+    ERROR
+)
+
+//create log map with level/logger associations
+func InitLog(path string) map[int]*log.Logger {
+    file, err := os.OpenFile(path, os.O_RDWR | os.O_CREATE | os.O_APPEND, 0666)
+    if err != nil {
+        panic(err)
+    }
+    logs := make(map[int]*log.Logger)
+
+    logs[INFO] = log.New(file, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
+    logs[WARNING] = log.New(file, "WARNING: ", log.Ldate|log.Ltime|log.Lshortfile)
+    logs[ERROR] = log.New(file, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
+
+    return logs
+}
