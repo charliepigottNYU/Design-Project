f9145b75b648bd3312602f02745d568deeee4cbe
Brian Quinn <bjq207@nyu.edu>
2017-11-14 14:38:54 -0500

    shell/sql for adding song + necessary updates

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,27 +1,33 @@
+DROP TABLE IF EXISTS modification_info;
+DROP TABLE IF EXISTS song_contributers;
+DROP TABLE IF EXISTS song_info;
+DROP TABLE IF EXISTS user_info;
+
+
 CREATE TABLE user_info(
-    username        TEXT PRIMARY KEY    NOT NULL,
-    password        TEXT                NOT NULL
+    username    TEXT PRIMARY KEY    NOT NULL,
+    password    TEXT                NOT NULL
 
 );
 
 CREATE TABLE song_info(
-    song_path       TEXT                NOT NULL,
-    username        TEXT                NOT NULL,
+    song_path   TEXT    UNIQUE NOT NULL,
+    username    TEXT           NOT NULL,
     PRIMARY KEY (song_path, username),
     FOREIGN KEY (username) REFERENCES user_info (username)
 );
 
 
 CREATE TABLE modification_info(
-    modification_path       TEXT PRIMARY KEY    NOT NULL,
-    original_path           TEXT                NOT NULL,
-    modifier                TEXT                NOT NULL,
+    modification_path   TEXT PRIMARY KEY    NOT NULL,
+    original_path       TEXT                NOT NULL,
+    modifier            TEXT                NOT NULL,
     FOREIGN KEY(original_path) REFERENCES song_info(song_path)
 );
 
 CREATE TABLE song_contributers(
-    song_path       TEXT        NOT NULL,
-    contributer     TEXT        NOT NULL,
+    song_path       TEXT    NOT NULL,
+    contributer     TEXT    NOT NULL,
     PRIMARY KEY(song_path, contributer),
     FOREIGN KEY(song_path) REFERENCES song_info(song_path)
 );
